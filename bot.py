646ad40ea5f05c339d0330d0f59aa538cb85f716
brandonnly <brandon@brandonly.me>
2021-01-16 00:47:38 -0500

    basic upload file to local

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import discord
 from discord.ext import commands
@@ -19,8 +20,25 @@ bot = discord.ext.commands.Bot(command_prefix=prefix,
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send("pong!")
+async def graph(ctx):
+    await ctx.send(f"Send a file to be a graphed. It must be a {', '.join(supported_file_types)}")
+
+    def check(message):
+        return message.author == ctx.author and len(message.attachments) == 1
+
+    msg = discord.Message
+    try:
+        msg = await bot.wait_for('message', check=check, timeout=60)
+    except asyncio.TimeoutError:
+        await ctx.send('Timed out. Please try again')
+
+    # check if file is supported
+    # FIXME: check for all in the supported file types list, not just .csv
+    if ".csv" not in msg.attachments[0].filename:
+        await ctx.send(f"That's not a valid file type. It must be a {', '.join(supported_file_types)}")
+    else:
+        # saves with filename
+        await msg.attachments[0].save("file.csv")
 
 
 bot.run(os.getenv('BOT_TOKEN'))
