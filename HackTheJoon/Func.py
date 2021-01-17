9c20096ef2bb04e07734cf07ea6d59c6e0adedf8
Jer7199 <jeromephilip03@gmail.com>
2021-01-17 02:31:45 -0500

    skip first line

--- HackTheJoon/Func.py
+++ HackTheJoon/Func.py
@@ -24,6 +24,7 @@ def basic_line(file):
 
     with open(file, 'r') as csvfile:
         plots = csv.reader(csvfile, delimiter=',')
+        # Skip first line
         next(plots)
         for row in plots:
             for j in range(1, len(row)):
